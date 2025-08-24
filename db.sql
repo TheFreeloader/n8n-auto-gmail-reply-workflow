7d5337afd20cee8dd1889d214e91573d95e5b94a
Cyryl John Navarro <cyryljohnn@gmail.com>
2025-08-24 16:21:02 +0800

    Remove database table definition for resend_auto_replies

--- db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS resend_auto_replies (
-  fingerprint TEXT PRIMARY KEY UNIQUE NOT NULL,
-  reply_to TEXT,
-  subject TEXT,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
-);
